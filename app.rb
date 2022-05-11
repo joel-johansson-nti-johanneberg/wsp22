a0b19654879095300b476b78b12a89bc31622f19
joel-johansson-nti-johanneberg <74712772+joel-johansson-nti-johanneberg@users.noreply.github.com>
2022-05-11 11:12:13 +0200

    :)

--- app.rb
+++ app.rb
@@ -5,57 +5,59 @@ require 'bcrypt'
 enable :sessions
 require_relative './model.rb'
 include Model
-
+# Attempts to check if the client has authorization
 before do
   if session[:auth] == nil && (request.path_info != '/login' && request.path_info != '/users/new' && request.path_info != '/error' && request.path_info != '/' && request.path_info != '/showlogin' && request.path_info != '/register' )
     redirect('/error')
   end
 end
-
+  # Attempts to check if restaurant exist
+before('/restaurant/:id') do
+  id = params[:id]
+  if not restaurant_exist(id)
+    redirect('/error_authorization')
+  end
+end
+# Attempts to check if the client has authorization
 before('/restaurant/:id/edit') do
   if session[:id] != 1 
     redirect('/error_authorization')
   end
 end
-
-=begin
-before('/restaurant/:id') do
-  id = params[:id]
-  if not dish_exist(id)
-    redirect('/error_authorization')
-  end
-end
-=end 
-
+# Displays an error message
 get('/error') do
   slim(:error)
 end
+# Displays an error message
 get('/error_empty') do
   slim(:error_empty)
 end
+# Displays an error message
 get('/error_authorization') do
   slim(:error_authorization)
 end
+# Displays an error message
 get('/error_restaurang_relation') do
   slim(:error_restaurang_relation)
 end 
+# Display Landing Page
 get('/')  do
   slim(:start)
 end 
-
+# Displays a register form
 get('/register') do
   slim(:register)
 end
-
+# Displays Landing Page from logout
 get('/logout') do
   session[:auth] = nil 
   redirect('/')
 end 
-
+# Displays a login form
 get('/showlogin') do
   slim(:login)
 end
-
+# Login 
 post('/login') do
   if session[:timeLogged] == nil
     session[:timeLogged] = 0
@@ -70,7 +72,10 @@ post('/login') do
     redirect('/showlogin')
   end
 end
-
+# Creates new user
+# @param [String] username, the user username
+# @param [String] password, the user password
+# @param [Hash] result, all information of a specific user
 post('/users/new') do
   if session[:timeLogged] == nil
     session[:timeLogged] = 0
@@ -86,40 +91,39 @@ post('/users/new') do
     redirect('/showlogin')
   end
 end
-
-
+# Displays a list of all restaurants
 get('/restaurants') do 
   restaurants_get()
 end 
-
+# Displays all dishes according to the filter
 get('/eaten_dishes/show') do 
   person = session[:id]
   result = eaten_dishes_show_get(person) 
   slim(:"person_dishes", locals:{result:result})
 end
-
+# Displays a form to post new dish
 get('/dishes/new')do 
   restaurants = restaurants_data()
   slim(:"dishes/new",locals:{restaurants:restaurants})
 end
-
+# Deletes an existing dish
 post('/dishes/:id/delete') do
   id = params[:id].to_i
   dish_delete_post(id)
 end
-
+# Creates a new dish
 post('/dishes/') do
   name = params[:name]
   type_of_food_id = params[:type_of_food_id].to_i
   where_id = params[:where_id].to_i
   dish_new_post(name,type_of_food_id,where_id)
 end 
-
+# Displays a edit dish  form
 get('/dishes/:id/edit') do
   id = params[:id].to_i
   dish_edit_get(id)
 end
-
+# Updates an existing dish
 post('/dishes/:id/update') do
   id = params[:id].to_i
   name = params[:name]
@@ -127,29 +131,29 @@ post('/dishes/:id/update') do
   type_of_food_id = params[:type_of_food_id].to_i
   dish_edit_post(id,name,where_id,type_of_food_id)
 end
-
+# Displays a edit restaurant form
 get('/restaurant/:id/edit') do
   id = params[:id].to_i
   restaurant_edit_get(id)
 end
-
+# Updates an existing restaurant
 post('/restaurant/:id/update') do
   id = params[:id].to_i
   name = params[:name]
   link = params[:link]
   restaurant_edit_post(id,name,where_id,link)
 end
-
+# Deletes an existing restaurant
 post('/restaurant/:id/delete') do
   id = params[:id].to_i
   restaurant_delete_post(id)
 end
-
+# Displays a list of all type of food at the selected restant
 get('/restaurant/:id') do
   restaurant_data = params[:id]
   resturant_id_get(restaurant_data) 
 end
-
+# Displays a list of all dishes at the selected type of food at the selected restaurant
 get("/restaurant/:resturant_id/type_of_food/:id") do
   restaurant_data = params[:resturant_id]
   type_of_food_data = params[:id].to_i
--- model.rb
+++ model.rb
@@ -1,15 +1,21 @@
 module Model
-
+    # Attempts to open a new database connection
+    # @return [Array] containing all the data from the database
     def connect_to_db()
         db = SQLite3::Database.new("db/database.db")
         db.results_as_hash = true
         return db
     end
+     #Attempts to select all restaurants
+    # @return [Array] containing all the restaurants from the database
+    # @see Model#connect_to_db
     def restaurants_data()
         db = connect_to_db()
         restaurants = db.execute("SELECT * FROM restaurant")
         return restaurants
     end 
+     # Attempts to check if user can login
+     # @see Model#connect_to_db
     def login_post(username, password)
         db = connect_to_db()
         result = db.execute("SELECT * FROM user WHERE username = ?",username).first
@@ -29,15 +35,31 @@ module Model
         redirect("https://www.youtube.com/watch?v=D-NJOoLlNC4")
         end
     end
-    def dish_exist(id) 
+    # Attempts to check if restaurant exist
+    # @param [Hash] exist, id for selected restaurant
+    # @see Model#connect_to_db
+    def restaurant_exist(id) 
         db = connect_to_db()
-        exist = db.execute("SELECT id FROM dishes WHERE id = ?",id).first
-        return exist == id
+        exist = db.execute("SELECT id FROM restaurant WHERE id = ?",id).first
+        if exist == nil
+            return false
+        else id = id.to_i
+            return exist["id"] == id
+        end 
     end 
+    # Attempts to check if too many inputs are recieved in close proximity
+    # @param [String] latestTime, the latest logged time
+    # @return [Boolean] whether the inputs are recieved in close proximity
+    # @see Model#connect_to_db
     def timeChecker(latestTime)
         timeDiff = Time.now.to_i - latestTime
         return timeDiff > 1.5
     end
+    # Attempts to register user
+    # @param [String] password, the password input
+    # @param [String] username, the user username
+    # @return [Boolean] whether the user registration succeeds 
+    # @see Model#connect_to_db
     def user_new_post( username,password,password_confirm) 
         if (password == password_confirm && username.length <= 12 && (isEmpty(username) == false) && (isEmpty(password)== false ))
         password_digest = BCrypt::Password.create(password)
@@ -45,13 +67,18 @@ module Model
         db = connect_to_db()
         db.execute("INSERT INTO user (username,pwdigest,authorization) VALUES (?,?,?)",username,password_digest,authorization)
         session[:auth] = true
+        result = db.execute("SELECT * FROM user WHERE username = ?",username).first
+        id = result["id"]
+        session[:id] = id
         redirect('/')
         else 
         session[:auth] = false
         "Användarnamnet är för långt/Lösenorden matchade inte/tomma fält"
         end
     end
-
+    # Attempts to delete a dish
+    # @param [Integer] id, the dish ID
+    # @see Model#connect_to_db
     def dish_delete_post(id)
         db = connect_to_db()
         result = db.execute("SELECT * FROM dishes WHERE id = ?",id).first
@@ -65,7 +92,7 @@ module Model
             redirect('/error_authorization')  
         end 
     end
-
+    # Attempts to se if text is empty
     def isEmpty(text)
         if text == nil
             return true
@@ -75,7 +102,9 @@ module Model
             return false
         end
     end
-
+    # Attempts to creat new dish
+    # @param [Integer] eaten by, the users ID
+    # @see Model#connect_to_db
     def dish_new_post(name,type_of_food_id,where_id)
         db = connect_to_db()
         if not isEmpty(name)
@@ -92,7 +121,10 @@ module Model
             redirect('/error_empty')
         end
     end 
-
+    # Attempts to update dish information
+    # @param [Hash] result , all information abaut the dish
+    # @param [Integer] eaten by, the users ID
+    # @see Model#connect_to_db
     def dish_edit_post(id,name,where_id,type_of_food_id)
         db = connect_to_db()
         result = db.execute("SELECT * FROM dishes WHERE id = ?",id).first
@@ -125,7 +157,8 @@ module Model
             redirect('/error_authorization')  
         end 
     end
-
+    # Attempts to update restaurant information
+    # @see Model#connect_to_db
     def restaurant_edit_post(id,name,where_id,link) 
         db = connect_to_db()
         if session[:id] == 1
@@ -135,20 +168,23 @@ module Model
             redirect('/error_authorization')  
         end 
     end
-
+    # Attempts to create a list of all restaurants
+    # @see Model#connect_to_db
     def restaurants_get()
         db = connect_to_db()
         restaurants = db.execute("SELECT * FROM restaurant")
         slim(:"restaurants/index", locals:{restaurants:restaurants})
     end 
-
+    # Attempts to create a list eaten dishes by user
+    # @see Model#connect_to_db
     def eaten_dishes_show_get(person)
         db = connect_to_db()
         result = db.execute("SELECT * FROM dishes WHERE eaten_by = ?",person)
         p result
         return result
     end
-
+    # Attempts show form to edit dish
+    # @see Model#connect_to_db
     def dish_edit_get(id)
         db = connect_to_db()
         result = db.execute("SELECT * FROM dishes WHERE id = ?",id).first
@@ -160,7 +196,8 @@ module Model
             redirect('/error_authorization')  
         end 
     end
-
+    # Attempts show form to edit restaurant
+    # @see Model#connect_to_db
     def restaurant_edit_get(id)
         if session[:id] == 1
             db = connect_to_db()
@@ -170,7 +207,8 @@ module Model
             redirect('/error_authorization') 
         end
     end
-
+    # Attempts delete restaurant
+    # @see Model#connect_to_db
     def restaurant_delete_post(id)
         if session[:id] == 1
             db = connect_to_db()
@@ -181,7 +219,8 @@ module Model
             redirect('/error_authorization')  
         end   
     end 
-
+    # Attempts show list of all types of food at the selected restaurant
+    # @see Model#connect_to_db
     def resturant_id_get(restaurant_data) 
         session[:resturant_id]=params[:id]
         db = connect_to_db()
@@ -189,7 +228,8 @@ module Model
         type_of_food = db.execute("SELECT * FROM (type_of_food_restaurant_relation INNER JOIN type_of_food ON type_of_food_restaurant_relation.type_of_food_id = type_of_food.id) WHERE restaurant_id = ?",restaurant_data )
         slim(:"restaurant/index", locals:{type_of_food:type_of_food,restaurant_id:restaurant_data,restaurant:restaurant})
     end
-
+    # Attempts show list all dishes ät the selected types of food at the selected restaurant
+    # @see Model#connect_to_db
     def restaurant_resturant_id_type_of_food_id_get(restaurant_data,type_of_food_data)
         db = connect_to_db()
         type_of_food_name = db.execute("SELECT * FROM type_of_food WHERE id = ?",type_of_food_data ).first
